cadcdd7779857fcadca9465a1f0e87efeb2972f2
ktc <ktc1205@gmail.com>
2025-11-21 07:23:24 +0900

    무중단 배포 구성 sh파일 오타로 재커밋3

--- scripts/start.sh
+++ scripts/start.sh
@@ -8,12 +8,14 @@ REPOSITORY=/home/ec2-user/app/step3
 PROJECT_NAME=freelec-springboot2-webservice
 
 echo "> Build 파일 복사"
-echo "> cp $REPOSITORY/zip/*.jar $REPOSITORY/"
+#echo "> cp $REPOSITORY/zip/*.jar $REPOSITORY/"
+echo "> cp $REPOSITORY/zip/build/libs/*.jar $REPOSITORY/"
 
-cp $REPOSITORY/zip/*.jar $REPOSITORY/
+#cp $REPOSITORY/zip/*.jar $REPOSITORY/
+cp $REPOSITORY/zip/build/libs/*.jar $REPOSITORY/
 
 echo "> 새 애플리케이션 배포"
-JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | grep -v 'plain' | tail -n 1)
 
 echo "> JAR_NAME: $JAR_NAME"
 echo "> $JAR_NAME 에 실행권한 추가"
